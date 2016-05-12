d5678abea6798f5542efc49352a52024b2c13e1d
Pieter van der Merwe <vandermerwe.pieter@gmail.com>
2016-05-12 20:22:52 +0200

    chore: rails generate react_on_rails:install plus some JSCS and ESlint setup

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+// DO NOT REQUIRE jQuery or jQuery-ujs in this file!
+// DO NOT REQUIRE TREE!
+
+// CRITICAL that vendor-bundle must be BEFORE bootstrap-sprockets and turbolinks
+// since it is exposing jQuery and jQuery-ujs
+
+//= require vendor-bundle
+//= require app-bundle
+
